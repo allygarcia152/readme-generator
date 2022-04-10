2bc7b13dca02e9ea0836614d052b255716f79d60
AllyGarcia <allygarcia152@gmail.com>
2022-04-10 17:46:38 -0500

    add license badge links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,21 @@
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 const renderLicenseBadge = license => {
-
+  if (data.license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (data.license === 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else if (data.license === 'BSD 3-Clause') {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  } else if (data.license === 'GNU GPLv3'){
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  } else if (data.license === 'ISC'){
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+  } else if (data.license === 'WTFPL'){
+    return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)'
+  } else {
+    return ''
+  }
 };
 
 // TODO: Create a function that returns the license link
@@ -15,7 +29,7 @@ function renderLicenseSection(license) {}
 // Create a function to generate markdown for README
 const generateMarkdown = data => {
   return `
-  # ${data.title}
+  # ${data.title} ${renderlicenseBadge(license)}
 
   ## Description
   ${data.description}
@@ -44,6 +58,8 @@ const generateMarkdown = data => {
   If you have any questions regarding this project, you can explore my repository at [github.com/${data.username}](www.github.com/${data.username}), and for further question, you can contact me via email at ${data.email}.
 
   ## License
+
+  
 
   `;
 };
